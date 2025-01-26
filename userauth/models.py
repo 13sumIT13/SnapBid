7ceb8b9d3dc67d1c5e69bfc816138d2558983c5e
sumitbansode360 <sumitbansode360@gmail.com>
2025-01-26 18:51:27 +0530

    user model created

---
+++ userauth/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    USERNAME_FIELD = 'username'
+
+    
+    def __str__(self):
+        self.first_name + ' ' + self.last_name
+    
