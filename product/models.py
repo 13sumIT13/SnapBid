819c97786db5167046c48b24ad2eec45301d5c21
sumitbansode360 <sumitbansode360@gmail.com>
2025-01-27 22:22:40 +0530

    migrations apply

--- product/models.py
+++ product/models.py
@@ -1,24 +1,22 @@
 from django.db import models
 import shortuuid
 
-# Choices for product status
 STATUS_CHOICES = [
-    ('Available', 'Available'),
-    ('Sold', 'Sold'),
+    ('A', 'Available'),
+    ('S', 'Sold'),
 ]
 
 class Product(models.Model):
-    # Unique identifier for the product
     p_id = models.CharField(
-        max_length=22,  # Default length for shortuuid
-        unique=True,
-        default=shortuuid.uuid  # Automatically generate a short UUID for new products
+        max_length=22, 
+        unique=True, 
+        default=shortuuid.uuid
     )
     name = models.CharField(max_length=100)
     description = models.TextField()
     starting_price = models.IntegerField()
     image = models.ImageField(upload_to='product_images')
-    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default='Available')
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1)
 
     def __str__(self):
         return self.name
