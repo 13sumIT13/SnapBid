2168fbc4121b2f0c1fb35a8600ce276d594138a8
sumitbansode360 <sumitbansode360@gmail.com>
2025-01-27 23:06:22 +0530

    authentication done

--- product/views.py
+++ product/views.py
@@ -1,36 +1,41 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from product.models import Product
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 class ProductList(ListView):
     model = Product
     template_name = 'product/product_list.html'
     context_object_name = 'products'
-
-class ProductDetail(DetailView):
+    
+class ProductDetail(LoginRequiredMixin, DetailView):
     model = Product
     template_name = 'product/product_detail.html'
     context_object_name = 'product'
     pk_url_kwarg = 'id'
+    login_url = 'login'
 
-class PrdocutCreate(CreateView):
+class PrdocutCreate(LoginRequiredMixin, CreateView):
     model = Product
     fields = "__all__"
     template_name = 'product/product_create.html'
     success_url = reverse_lazy("product-list")
+    login_url = 'login'
 
-class ProductUpdate(UpdateView):
+class ProductUpdate(LoginRequiredMixin, UpdateView):
     model = Product
     fields = "__all__"
     success_url = reverse_lazy("product-list")
     template_name = 'product/product_update.html'
     pk_url_kwarg = 'id'
+    login_url = 'login'
 
 
-class ProductDelete(DeleteView):
+class ProductDelete(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'product/product_delete.html'
     success_url = reverse_lazy("product-list")
     pk_url_kwarg = 'id'
+    login_url = 'login'
     
