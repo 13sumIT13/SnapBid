73e02e733440ea328e16e02ee4b9652a386b4fa4
sumitbansode360 <sumitbansode360@gmail.com>
2025-01-28 13:55:51 +0530

    auction model created

--- product/models.py
+++ product/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-import shortuuid
+from userauth.models import User
 
 STATUS_CHOICES = [
     ('A', 'Available'),
     ('S', 'Sold'),
 ]
+
+BID_CHOICES = [
+    ('Live', 'Live'),
+    ('Closed', 'Closed'),
+]
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -15,3 +21,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Auction(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    current_bid = models.IntegerField()
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(choices=BID_CHOICES, max_length=10)
